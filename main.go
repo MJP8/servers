ca7d967c050ce3786da50a8f8e8851e96a6a26f0
MJP8 <malachi@timelapseplus.com>
2022-11-12 17:23:10 -0600

    add more example features

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 import (
 	s "github.com/MJP8/servers/http"
 	"time"
+	"os"
+	"fmt"
 )
 type Day struct {
 	Hour int
@@ -12,6 +14,17 @@ func main() {
 	day.Hour, day.Minute, _ = time.Now().Clock() // populate the struct with the current time
 	template := s.NewTemplate("examples/templates/index.html", day)
 	s.Init(4040) // initiate the server
+	s.HandleCustom("/save/", func(w s.ResponseWriter, r *s.Request) {
+		r.ParseForm()
+		saveTime := r.Form.Get("time")
+		err := os.WriteFile("examples/output/time.txt", []byte(saveTime), 0600)
+		if err != nil {
+			serr := s.Error(s.InternalServerError, err.Error(), w)
+			err = serr.ToErr()
+			fmt.Printf("%v", err)
+		}
+		fmt.Fprintf(w, "<script>location.assign(\"/\");</script>") // redirect to "/"
+	})
 	s.HandleTemplate("/", template) // add a template handler for the path "/"
 	s.HandleStaticFile("/styles.css", "examples/static/styles.css", "text/css") // add a static handler for "/styles.css"
 	s.Serve() // start the server
